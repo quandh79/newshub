6bee4a8df358ab14b5fa03cd4cb9a5eb40151ed5
quandh79 <dohongquantc@gmail.com>
2023-03-01 21:01:57 +0700

    1

---
+++ resources/views/frontend/manager/link/js.blade.php
@@ -0,0 +1,45 @@
+ <!-- ==== jQuery Library ==== -->
+ <script src="{{ asset('frontendfrontend/js/jquery-3.2.1.min.js') }}"></script>
+
+ <!-- ==== Bootstrap Framework ==== -->
+ <script src="{{ asset('frontendfrontend/js/bootstrap.min.js') }}"></script>
+
+ <!-- ==== StickyJS Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/jquery.sticky.min.js') }}"></script>
+
+ <!-- ==== HoverIntent Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/jquery.hoverIntent.min.js') }}"></script>
+
+ <!-- ==== Marquee Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/jquery.marquee.min.js') }}"></script>
+
+ <!-- ==== Validation Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/jquery.validate.min.js') }}"></script>
+
+ <!-- ==== Isotope Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/isotope.min.js') }}"></script>
+
+ <!-- ==== Resize Sensor Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/resizesensor.min.js') }}"></script>
+
+ <!-- ==== Sticky Sidebar Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/theia-sticky-sidebar.min.js') }}"></script>
+
+ <!-- ==== Zoom Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/jquery.zoom.min.js') }}"></script>
+
+ <!-- ==== Bar Rating Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/jquery.barrating.min.js') }}"></script>
+
+ <!-- ==== Countdown Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/jquery.countdown.min.js') }}"></script>
+
+ <!-- ==== RetinaJS Plugin ==== -->
+ <script src="{{ asset('frontendfrontend/js/retina.min.js') }}"></script>
+
+ <!-- ==== Google Map API ==== -->
+ <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBK9f7sXWmqQ1E-ufRXV3VpXOn_ifKsDuc"></script>
+
+ <!-- ==== Main JavaScript ==== -->
+ <script src="{{ asset('frontendfrontend/js/main.js') }}"></script>
+ @yield('jsfrontend')
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title')
+    <title>NewsHub - Trang quản trị</title>
+@endsection
+
